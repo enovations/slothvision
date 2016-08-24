891f1cedf8dd6a31d4c19ae86eb77d99849d6bbc
zigapk <ziga.patacko@gmail.com>
2016-08-24 16:59:25 +0200

    add pi camera classes

---
+++ src/com/sourcegasm/slothvision/picamera/PiCamera.java
@@ -0,0 +1,47 @@
+package com.sourcegasm.slothvision.picamera;
+
+import java.net.InetAddress;
+import java.net.InterfaceAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
+
+public class PiCamera {
+    private PiConnectorInstance instance1 = null;
+    private PiConnectorInstance instance2 = null;
+
+    public PiCamera(String netInterface) {
+        try {
+            instance1 = new PiConnectorInstance("192.168.0.50");
+        }catch (Exception e){
+            System.out.println("COULD NOT CONNECT TO RBPI1 (192.168.0.50)");
+        }
+        try {
+            instance2 = new PiConnectorInstance("192.168.0.51");
+        }catch (Exception e){
+            System.out.println("COULD NOT CONNECT TO RBPI2 (192.168.0.51)");
+        }
+        try {
+            String ip = getIp("enp0s25");
+            instance1.setIPAddress(ip);
+            instance2.setIPAddress(ip);
+        }catch (Exception e){
+            System.out.println("COUDL NOT FIND IP FOR " + netInterface);
+        }
+    }
+
+    private static String getIp(String ifName) throws SocketException {
+        for (Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces(); en.hasMoreElements(); ) {
+            NetworkInterface intf = en.nextElement();
+            if (intf.getName().equals(ifName)) {
+                for (Enumeration<InetAddress> enumIpAddr = intf.getInetAddresses(); enumIpAddr.hasMoreElements(); ) {
+                    String asdf = enumIpAddr.nextElement().toString();
+                    if (asdf.contains(".")) {
+                        return asdf.substring(1);
+                    }
+                }
+            }
+        }
+        throw new SocketException();
+    }
+}
---
+++ src/com/sourcegasm/slothvision/picamera/PiConnectorInstance.java
@@ -0,0 +1,64 @@
+package com.sourcegasm.slothvision.picamera;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+public class PiConnectorInstance {
+    private String host = "192.168.0.100";
+    private int port = 8008;
+
+    private DatagramSocket sock;
+    private InetAddress IPAddress;
+
+    public PiConnectorInstance(String host) throws Exception {
+        this.host = host;
+        connect();
+    }
+
+    public PiConnectorInstance(String host, int port) throws Exception {
+        this.host = host;
+        this.port = port;
+        connect();
+    }
+
+    private void connect() throws Exception {
+        sock = new DatagramSocket();
+        IPAddress = InetAddress.getByName(host);
+    }
+
+    private void sendMessage(String msg) {
+        try {
+            msg += "\n";
+            byte[] sendData = msg.getBytes();
+            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
+            sock.send(sendPacket);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void setIPAddress(String add) {
+        sendMessage("ip: " + add);
+    }
+
+    public void setPort(int port) {
+        sendMessage("port: " + port);
+    }
+
+    public void run() {
+        kill();
+        sendMessage("run");
+    }
+
+    public void run(int port) {
+        kill();
+        setPort(port);
+        sendMessage("run");
+    }
+
+    public void kill() {
+        sendMessage("kill");
+    }
+}
