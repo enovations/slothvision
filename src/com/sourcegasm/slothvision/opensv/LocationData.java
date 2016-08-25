a99123d9eef470a619de6c7f503c3c4c97976c3d
zigapk <ziga.patacko@gmail.com>
2016-08-25 16:23:54 +0200

    added marker recognition

---
+++ src/com/sourcegasm/slothvision/opensv/LocationData.java
@@ -0,0 +1,11 @@
+package com.sourcegasm.slothvision.opensv;
+
+public class LocationData {
+    public int x = 0;
+    public int y = 0;
+
+    public LocationData(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+}
