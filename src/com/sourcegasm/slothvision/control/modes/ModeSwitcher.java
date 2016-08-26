4cc64ca641388836fe0c93c626c367311876e957
zigapk <ziga.patacko@gmail.com>
2016-08-26 10:28:50 +0200

    add pan maker tracker

--- src/com/sourcegasm/slothvision/control/modes/ModeSwitcher.java
+++ src/com/sourcegasm/slothvision/control/modes/ModeSwitcher.java
@@ -4,34 +4,36 @@ import com.sourcegasm.slothvision.Launcher;
 
 public class ModeSwitcher {
 
-	private int mode = 0;
+    private int mode = 0;
 
-	public void nextMode() {
+    public void nextMode() {
 
-		int maxMode = 2;
-		if (mode == maxMode) {
-			mode = 0;
-		} else {
-			mode++;
-		}
+        int maxMode = 3;
+        if (mode == maxMode) {
+            mode = 0;
+        } else {
+            mode++;
+        }
 
-		Launcher.piROSConnector.data.mode = mode;
+        Launcher.piROSConnector.data.mode = mode;
 
-	}
+    }
 
-	public static String getModeName(int mode) {
+    public static String getModeName(int mode) {
 
-		switch (mode) {
-		case 0:
-			return "manual acc";
-		case 1:
-			return "manual fix";
-		case 2:
-			return "follow pt";
-		default:
-			return "unknown";
-		}
+        switch (mode) {
+            case 0:
+                return "manual acc";
+            case 1:
+                return "manual fix";
+            case 2:
+                return "follow pt";
+            case 3:
+                return "adv follow";
+            default:
+                return "unknown";
+        }
 
-	}
+    }
 
 }
