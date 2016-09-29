a6ed8d5cf3617e7ece9402c2a60350a62e75e8a6
zigapk <ziga.patacko@gmail.com>
2016-09-29 09:51:36 +0200

    edit conf and add udp class

---
+++ udp.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "udp.h"
+//#include "boost/asio.hpp"
+
+//using namespace boost::asio;
+
+
+int neki() {
+    /*io_service io_service;
+    ip::udp::socket socket(io_service);
+    ip::udp::endpoint remote_endpoint;
+
+    socket.open(ip::udp::v4());
+
+    remote_endpoint = ip::udp::endpoint(ip::address::from_string("192.168.0.4"), 9000);
+
+    boost::system::error_code err;
+    socket.send_to(buffer("Jane Doe", 8), remote_endpoint, 0, err);
+
+    socket.close();*/
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
---
+++ udp.h
@@ -0,0 +1,10 @@
+#ifndef SLOTHVISION_UDP_H
+#define SLOTHVISION_UDP_H
+
+
+class udp {
+
+};
+
+
+#endif //SLOTHVISION_UDP_H
