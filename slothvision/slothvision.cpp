67636119fee7eb2d74ee1649a3b96833c0b3c6e9
Klemen <klemen.pylo@gmail.com>
2016-09-30 11:39:57 +0200

    sgsd

--- slothvision/slothvision.cpp
+++ slothvision/slothvision.cpp
@@ -1,10 +1,27 @@
 #include <Windows.h>
 #include "render/render.h"
 #include "network/CameraStream.h"
+#include "network/NetworkManager.h"
 
+network_manager::IPv4 raspicam1;
+network_manager::IPv4 raspicam2;
 
 int WINAPI WinMain(HINSTANCE hinst, HINSTANCE, LPSTR, int)
 {
+
+	raspicam1.b1 = 192;
+	raspicam1.b2 = 168;
+	raspicam1.b3 = 0;
+	raspicam1.b4 = 50;
+
+	raspicam2.b1 = 192;
+	raspicam2.b2 = 168;
+	raspicam2.b3 = 0;
+	raspicam2.b4 = 51;
+
+	network_manager::MSG_requestCameraVideoData(5000, raspicam1);
+	network_manager::MSG_requestCameraVideoData(5001, raspicam2);
+
 	Render render;
 	
 	CameraStream leftCameraStream(5000);
@@ -15,8 +32,6 @@ int WINAPI WinMain(HINSTANCE hinst, HINSTANCE, LPSTR, int)
 
 	render.setLeftCameraStream(&leftCameraStream); 
 	render.setRightCameraStream(&rightCameraStream);
-
-
 
 	//last thing to be called
 	render.start(hinst);
