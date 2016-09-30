c04bf0197c1493bd4ca7373fe8a0b38c6931dd87
zigapk <ziga.patacko@gmail.com>
2016-09-30 12:18:29 +0200

    add parser

--- slothvision/slothvision.cpp
+++ slothvision/slothvision.cpp
@@ -1,15 +1,30 @@
 #include <Windows.h>
 #include "render/render.h"
 
-int WINAPI WinMain(HINSTANCE hinst, HINSTANCE, LPSTR, int)
+int WINAPI
+WinMain(HINSTANCE
+hinst, HINSTANCE, LPSTR, int)
 {
 
-	Render render;
-	
+/*Render render;
 
 
-	//last thing to be called
-	render.start(hinst);
 
-	return(0);
+//last thing to be called
+render.start(hinst);*/
+
+int nogavica = connect(8008);
+uint32_t add = 0;
+uint8_t *tmp = (uint8_t * ) & add;
+tmp[0] = 192;
+tmp[1] = 168;
+tmp[2] = 0;
+tmp[3] = 100;
+sendData(add, nogavica,
+"neki", 4);
+disconnect(nogavica);
+
+TerminateWinsock();
+
+return(0);
 }
