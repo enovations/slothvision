2d0820438ff3ff4c220341b5c46e38c45ddce4b8
Andrej <andrej.zdesar@fe.uni-lj.si>
2016-10-03 00:15:56 +0200

    Marker tracking code enabled.

--- slothvision/marker/marker.cpp
+++ slothvision/marker/marker.cpp
@@ -1,14 +1,16 @@
-/*#include "marker.h"
+#include "marker.h"
 
 void marker::generateMarker(const std::string& path) {
     cv::Mat markerImage;
-    cv::Ptr<cv::aruco::Dictionary> dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_6X6_250);
+    //cv::Ptr<cv::aruco::Dictionary> dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_6X6_250);
+	cv::aruco::Dictionary dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_6X6_250);
     cv::aruco::drawMarker(dictionary, 23, 600, markerImage, 1);
     cv::imwrite(path, markerImage);
 }
 
 bool marker::markerPosition(const cv::Mat& image, cv::Point2f& center, std::vector<cv::Point2f>& corners) {
-    cv::Ptr<cv::aruco::Dictionary> dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_6X6_250);
+    //cv::Ptr<cv::aruco::Dictionary> dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_6X6_250);
+	cv::aruco::Dictionary dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_6X6_250);
     std::vector<int> ids;
     std::vector<std::vector<cv::Point2f>> foundCorners;
     cv::aruco::detectMarkers(image, dictionary, foundCorners, ids);
@@ -37,9 +39,10 @@ bool marker::markerPosition(const cv::Mat& image, cv::Point2f& center, std::vect
 }
 
 double marker::markerDistance(const cv::Point2f& left, const cv::Point2f& right) {
-    double distance = left.x - right.x;
-    distance = fabs(distance);
+    double disparity = left.x - right.x;
+	double distance = -1.0;
+	if ( disparity>0.1 )
+		distance = FB/disparity;
 
     return distance;
 }
-*/
--- slothvision/marker/marker.h
+++ slothvision/marker/marker.h
@@ -1,4 +1,4 @@
-/*#ifndef SLOTHVISION_MARKER_H
+#ifndef SLOTHVISION_MARKER_H
 #define SLOTHVISION_MARKER_H
 
 #include <iostream>
@@ -11,9 +11,11 @@
 #include <math.h>
 
 namespace marker {
+	static const double FB = 80.8; // Focal length (in pixels) and baseline (in m)
+	// FOV = atan(2592/2*0.0000014/0.0036) ~= 54, f = 2592/2/2/tan(54/2/180*pi), b = 0.0635, fb = 2592/2/2/tan(54/2/180*pi)*0.0635 ~= 80.8
     void generateMarker(const std::string& path);
     bool markerPosition(const cv::Mat& image, cv::Point2f& center, std::vector<cv::Point2f>& corners);
     double markerDistance(const cv::Point2f&, const cv::Point2f&);
 }
 
-#endif*/
+#endif
