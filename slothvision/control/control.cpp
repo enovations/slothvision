7bc9048b9561c735b767402cf9989c3c9eb95a8e
Andrej <andrej.zdesar@fe.uni-lj.si>
2016-10-03 06:01:15 +0200

    Manual control.

--- slothvision/control/control.cpp
+++ slothvision/control/control.cpp
@@ -27,6 +27,28 @@ void Control::stop() {
 
 void Control::sendUpdate() {
     while (running) {
+		bool go = sensors->isGo();
+		if (go) {
+			Sensors::Mode mode;
+			sensors->getMode(mode);
+			OVR::OvrPlatform::GamepadState state;
+			sensors->getGamepadState(state);
+			
+			if (mode.robot == Sensors::Mode::ROBOT_MANUAL) {
+				speed = state.RY*0.1;
+				steer = state.RX*0.5;
+			}
+			
+			if (mode.panTilt == Sensors::Mode::PANTILT_MANUAL) {
+				pan = state.LX*7000;
+				tilt = state.LY*2000;
+			}
+		} else {
+			speed = 0;
+			steer = 0;
+			pan = 0;
+			tilt = 0;
+		}
 
         std::string robotMessage = "r " + std::to_string(speed) + " " + std::to_string(steer);
         network_manager::sendStringUDP(robotMessage, address, 8008);
